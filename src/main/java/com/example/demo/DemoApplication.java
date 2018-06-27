972175cf81597e376ba477461223000d18e39a96
wangtao-Allen <wangtao_333@icloud.com>
2018-06-27 17:12:43 +0800

    添加注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 连接微信公众号,通过开发平台接受发送消息
+ */
 @SpringBootApplication
 public class DemoApplication {
 
