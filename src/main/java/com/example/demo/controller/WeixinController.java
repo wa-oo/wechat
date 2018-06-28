c5bd404cc46df9114d2670e8bb40ec016409d126
wangtao-Allen <wangtao_333@icloud.com>
2018-06-28 10:08:07 +0800

    修改因为没有设置数据库连接报错

--- src/main/java/com/example/demo/controller/WeixinController.java
+++ src/main/java/com/example/demo/controller/WeixinController.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 
 @Controller
 @RequestMapping("/wechat")
-public class WechatController {
+public class WeixinController {
 
     private final String token = "wangtao";
 
